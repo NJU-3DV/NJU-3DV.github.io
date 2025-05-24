75a2b52f8a1dffa87370cb64cb7306fad875aafa
Youtian Lin <linyoutian.loyot@gmail.com>
2025-05-25 05:20:20 +0800

    Direct3D-S2

---
+++ projects/Direct3D-S2/static/js/scripts.js
@@ -0,0 +1,112 @@
+var videosAttributes = [{"caption": "GT Mesh", "mesh_src": "assets/samples/gt/wukongnorm.obj.drc", "src": "assets/videos/gt/wukongnorm.obj.mp4"}, {"caption": "Ours", "mesh_src": "assets/samples/ours/wukongnorm_recon.obj.drc", "src": "assets/videos/ours/wukongnorm_recon.obj.mp4"}, {"caption": "Tripo 2.5", "mesh_src": "assets/samples/tripo/wukongnorm.obj.obj.drc", "src": "assets/videos/tripo/wukongnorm.obj.obj.mp4"}, {"caption": "Dora", "mesh_src": "assets/samples/dora/wukongnorm.obj.drc", "src": "assets/videos/dora/wukongnorm.obj.mp4"}, {"caption": "Trellis", "mesh_src": "assets/samples/trellis/it0_wukongnorm.obj.drc", "src": "assets/videos/trellis/it0_wukongnorm.obj.mp4"}, {"mesh_src": "assets/samples/gt/bicycle_001.obj.drc", "src": "assets/videos/gt/bicycle_001.obj.mp4"}, {"mesh_src": "assets/samples/ours/bicycle_001_recon.obj.drc", "src": "assets/videos/ours/bicycle_001_recon.obj.mp4"}, {"mesh_src": "assets/samples/tripo/bicycle_001.obj.obj.drc", "src": "assets/videos/tripo/bicycle_001.obj.obj.mp4"}, {"mesh_src": "assets/samples/dora/bicycle_001.ply.obj.drc", "src": "assets/videos/dora/bicycle_001.ply.obj.mp4"}, {"mesh_src": "assets/samples/trellis/bicycle_001.obj.drc", "src": "assets/videos/trellis/bicycle_001.obj.mp4"}, {"mesh_src": "assets/samples/gt/flower_002.obj.drc", "src": "assets/videos/gt/flower_002.obj.mp4"}, {"mesh_src": "assets/samples/ours/flower_002_recon.obj.drc", "src": "assets/videos/ours/flower_002_recon.obj.mp4"}, {"mesh_src": "assets/samples/tripo/flower_002.obj.obj.drc", "src": "assets/videos/tripo/flower_002.obj.obj.mp4"}, {"mesh_src": "assets/samples/dora/flower_002.ply.obj.drc", "src": "assets/videos/dora/flower_002.ply.obj.mp4"}, {"mesh_src": "assets/samples/trellis/flower_002.obj.drc", "src": "assets/videos/trellis/flower_002.obj.mp4"}, {"mesh_src": "assets/samples/gt/archimedesnorm.obj.drc", "src": "assets/videos/gt/archimedesnorm.obj.mp4"}, {"mesh_src": "assets/samples/ours/archimedesnorm_recon.obj.drc", "src": "assets/videos/ours/archimedesnorm_recon.obj.mp4"}, {"mesh_src": "assets/samples/tripo/archimedesnorm.obj.obj.drc", "src": "assets/videos/tripo/archimedesnorm.obj.obj.mp4"}, {"mesh_src": "assets/samples/dora/archimedesnorm.obj.drc", "src": "assets/videos/dora/archimedesnorm.obj.mp4"}, {"mesh_src": "assets/samples/trellis/it0_archimedesnorm.obj.drc", "src": "assets/videos/trellis/it0_archimedesnorm.obj.mp4"}, {"mesh_src": "assets/samples/gt/jacketnorm.obj.drc", "src": "assets/videos/gt/jacketnorm.obj.mp4"}, {"mesh_src": "assets/samples/ours/jacketnorm_recon.obj.drc", "src": "assets/videos/ours/jacketnorm_recon.obj.mp4"}, {"mesh_src": "assets/samples/tripo/jacketnorm.obj.obj.drc", "src": "assets/videos/tripo/jacketnorm.obj.obj.mp4"}, {"mesh_src": "assets/samples/dora/jacketnorm.obj.drc", "src": "assets/videos/dora/jacketnorm.obj.mp4"}, {"mesh_src": "assets/samples/trellis/it0_jacketnorm.obj.drc", "src": "assets/videos/trellis/it0_jacketnorm.obj.mp4"}, {"mesh_src": "assets/samples/gt/tractor_003.obj.drc", "src": "assets/videos/gt/tractor_003.obj.mp4"}, {"mesh_src": "assets/samples/ours/tractor_003_recon.obj.drc", "src": "assets/videos/ours/tractor_003_recon.obj.mp4"}, {"mesh_src": "assets/samples/tripo/tractor_003.obj.obj.drc", "src": "assets/videos/tripo/tractor_003.obj.obj.mp4"}, {"mesh_src": "assets/samples/dora/tractor_003.ply.obj.drc", "src": "assets/videos/dora/tractor_003.ply.obj.mp4"}, {"mesh_src": "assets/samples/trellis/tractor_003.obj.drc", "src": "assets/videos/trellis/tractor_003.obj.mp4"}, {"mesh_src": "assets/samples/gt/bicycle_005.obj.drc", "src": "assets/videos/gt/bicycle_005.obj.mp4"}, {"mesh_src": "assets/samples/ours/bicycle_005_recon.obj.drc", "src": "assets/videos/ours/bicycle_005_recon.obj.mp4"}, {"mesh_src": "assets/samples/tripo/bicycle_005.obj.obj.drc", "src": "assets/videos/tripo/bicycle_005.obj.obj.mp4"}, {"mesh_src": "assets/samples/dora/bicycle_005.ply.obj.drc", "src": "assets/videos/dora/bicycle_005.ply.obj.mp4"}, {"mesh_src": "assets/samples/trellis/bicycle_005.obj.drc", "src": "assets/videos/trellis/bicycle_005.obj.mp4"}, {"mesh_src": "assets/samples/gt/loongnorm.obj.drc", "src": "assets/videos/gt/loongnorm.obj.mp4"}, {"mesh_src": "assets/samples/ours/loongnorm_recon.obj.drc", "src": "assets/videos/ours/loongnorm_recon.obj.mp4"}, {"mesh_src": "assets/samples/tripo/loongnorm.obj.obj.drc", "src": "assets/videos/tripo/loongnorm.obj.obj.mp4"}, {"mesh_src": "assets/samples/dora/loongnorm.obj.drc", "src": "assets/videos/dora/loongnorm.obj.mp4"}, {"mesh_src": "assets/samples/trellis/it0_loongnorm.obj.drc", "src": "assets/videos/trellis/it0_loongnorm.obj.mp4"}, {"mesh_src": "assets/samples/gt/2_gt.obj.drc", "src": "assets/videos/gt/2_gt.obj.mp4"}, {"mesh_src": "assets/samples/ours/2_reconstruction.obj.drc", "src": "assets/videos/ours/2_reconstruction.obj.mp4"}, {"mesh_src": "assets/samples/tripo/2_gt.obj.obj.drc", "src": "assets/videos/tripo/2_gt.obj.obj.mp4"}, {"mesh_src": "assets/samples/dora/2_gt.obj.drc", "src": "assets/videos/dora/2_gt.obj.mp4"}, {"mesh_src": "assets/samples/trellis/it0_2.obj.drc", "src": "assets/videos/trellis/it0_2.obj.mp4"}, {"mesh_src": "assets/samples/gt/mousenorm.obj.drc", "src": "assets/videos/gt/mousenorm.obj.mp4"}, {"mesh_src": "assets/samples/ours/mousenorm_recon.obj.drc", "src": "assets/videos/ours/mousenorm_recon.obj.mp4"}, {"mesh_src": "assets/samples/tripo/mousenorm.obj.obj.drc", "src": "assets/videos/tripo/mousenorm.obj.obj.mp4"}, {"mesh_src": "assets/samples/dora/mousenorm.obj.drc", "src": "assets/videos/dora/mousenorm.obj.mp4"}, {"mesh_src": "assets/samples/trellis/it0_mousenorm.obj.drc", "src": "assets/videos/trellis/it0_mousenorm.obj.mp4"}, {"mesh_src": "assets/samples/gt/3_gt.obj.drc", "src": "assets/videos/gt/3_gt.obj.mp4"}, {"mesh_src": "assets/samples/ours/3_reconstruction.obj.drc", "src": "assets/videos/ours/3_reconstruction.obj.mp4"}, {"mesh_src": "assets/samples/tripo/3_gt.obj.obj.drc", "src": "assets/videos/tripo/3_gt.obj.obj.mp4"}, {"mesh_src": "assets/samples/dora/3_gt.obj.drc", "src": "assets/videos/dora/3_gt.obj.mp4"}, {"mesh_src": "assets/samples/trellis/it0_3.obj.drc", "src": "assets/videos/trellis/it0_3.obj.mp4"}, {"mesh_src": "assets/samples/gt/1_gt.obj.drc", "src": "assets/videos/gt/1_gt.obj.mp4"}, {"mesh_src": "assets/samples/ours/1_reconstruction.obj.drc", "src": "assets/videos/ours/1_reconstruction.obj.mp4"}, {"mesh_src": "assets/samples/tripo/1_gt.obj.obj.drc", "src": "assets/videos/tripo/1_gt.obj.obj.mp4"}, {"mesh_src": "assets/samples/dora/1_gt.obj.drc", "src": "assets/videos/dora/1_gt.obj.mp4"}, {"mesh_src": "assets/samples/trellis/it0_1.obj.drc", "src": "assets/videos/trellis/it0_1.obj.mp4"}, {"mesh_src": "assets/samples/gt/motorcycle_013.obj.drc", "src": "assets/videos/gt/motorcycle_013.obj.mp4"}, {"mesh_src": "assets/samples/ours/motorcycle_013_recon.obj.drc", "src": "assets/videos/ours/motorcycle_013_recon.obj.mp4"}, {"mesh_src": "assets/samples/tripo/motorcycle_013.obj.obj.drc", "src": "assets/videos/tripo/motorcycle_013.obj.obj.mp4"}, {"mesh_src": "assets/samples/dora/motorcycle_013.ply.obj.drc", "src": "assets/videos/dora/motorcycle_013.ply.obj.mp4"}, {"mesh_src": "assets/samples/trellis/motorcycle_013.obj.drc", "src": "assets/videos/trellis/motorcycle_013.obj.mp4"}, {"mesh_src": "assets/samples/gt/4_gt.obj.drc", "src": "assets/videos/gt/4_gt.obj.mp4"}, {"mesh_src": "assets/samples/ours/4_reconstruction.obj.drc", "src": "assets/videos/ours/4_reconstruction.obj.mp4"}, {"mesh_src": "assets/samples/tripo/4_gt.obj.obj.drc", "src": "assets/videos/tripo/4_gt.obj.obj.mp4"}, {"mesh_src": "assets/samples/dora/4_gt.obj.drc", "src": "assets/videos/dora/4_gt.obj.mp4"}, {"mesh_src": "assets/samples/trellis/it0_4.obj.drc", "src": "assets/videos/trellis/it0_4.obj.mp4"}, {"mesh_src": "assets/samples/gt/parthenonnorm.obj.drc", "src": "assets/videos/gt/parthenonnorm.obj.mp4"}, {"mesh_src": "assets/samples/ours/parthenonnorm_recon.obj.drc", "src": "assets/videos/ours/parthenonnorm_recon.obj.mp4"}, {"mesh_src": "assets/samples/tripo/parthenonnorm.obj.obj.drc", "src": "assets/videos/tripo/parthenonnorm.obj.obj.mp4"}, {"mesh_src": "assets/samples/dora/parthenonnorm.obj.drc", "src": "assets/videos/dora/parthenonnorm.obj.mp4"}, {"mesh_src": "assets/samples/trellis/it0_parthenonnorm.obj.drc", "src": "assets/videos/trellis/it0_parthenonnorm.obj.mp4"}, {"mesh_src": "assets/samples/gt/lionnorm.obj.drc", "src": "assets/videos/gt/lionnorm.obj.mp4"}, {"mesh_src": "assets/samples/ours/lion_reconstruction.obj.drc", "src": "assets/videos/ours/lion_reconstruction.obj.mp4"}, {"mesh_src": "assets/samples/tripo/lionnorm.obj.obj.drc", "src": "assets/videos/tripo/lionnorm.obj.obj.mp4"}, {"mesh_src": "assets/samples/dora/lionnorm.obj.drc", "src": "assets/videos/dora/lionnorm.obj.mp4"}, {"mesh_src": "assets/samples/trellis/it0_lionnorm.obj.drc", "src": "assets/videos/trellis/it0_lionnorm.obj.mp4"}, {"mesh_src": "assets/samples/gt/keyboard_004.obj.drc", "src": "assets/videos/gt/keyboard_004.obj.mp4"}, {"mesh_src": "assets/samples/ours/keyboard_004_recon.obj.drc", "src": "assets/videos/ours/keyboard_004_recon.obj.mp4"}, {"mesh_src": "assets/samples/tripo/keyboard_004.obj.obj.drc", "src": "assets/videos/tripo/keyboard_004.obj.obj.mp4"}, {"mesh_src": "assets/samples/dora/keyboard_004.ply.obj.drc", "src": "assets/videos/dora/keyboard_004.ply.obj.mp4"}, {"mesh_src": "assets/samples/trellis/keyboard_004.obj.drc", "src": "assets/videos/trellis/keyboard_004.obj.mp4"}, {"mesh_src": "assets/samples/gt/5_gt.obj.drc", "src": "assets/videos/gt/5_gt.obj.mp4"}, {"mesh_src": "assets/samples/ours/5_reconstruction.obj.drc", "src": "assets/videos/ours/5_reconstruction.obj.mp4"}, {"mesh_src": "assets/samples/tripo/5_gt.obj.obj.drc", "src": "assets/videos/tripo/5_gt.obj.obj.mp4"}, {"mesh_src": "assets/samples/dora/5_gt.obj.drc", "src": "assets/videos/dora/5_gt.obj.mp4"}, {"mesh_src": "assets/samples/trellis/it0_5.obj.drc", "src": "assets/videos/trellis/it0_5.obj.mp4"}, {"mesh_src": "assets/samples/gt/archernorm.obj.drc", "src": "assets/videos/gt/archernorm.obj.mp4"}, {"mesh_src": "assets/samples/ours/archernorm_recon.obj.drc", "src": "assets/videos/ours/archernorm_recon.obj.mp4"}, {"mesh_src": "assets/samples/tripo/archernorm.obj.obj.drc", "src": "assets/videos/tripo/archernorm.obj.obj.mp4"}, {"mesh_src": "assets/samples/dora/archernorm.obj.drc", "src": "assets/videos/dora/archernorm.obj.mp4"}, {"mesh_src": "assets/samples/trellis/it0_archernorm.obj.drc", "src": "assets/videos/trellis/it0_archernorm.obj.mp4"}, {"mesh_src": "assets/samples/gt/lampnorm.obj.drc", "src": "assets/videos/gt/lampnorm.obj.mp4"}, {"mesh_src": "assets/samples/ours/lampnorm_recon.obj.drc", "src": "assets/videos/ours/lampnorm_recon.obj.mp4"}, {"mesh_src": "assets/samples/tripo/lampnorm.obj.obj.drc", "src": "assets/videos/tripo/lampnorm.obj.obj.mp4"}, {"mesh_src": "assets/samples/dora/lampnorm.obj.drc", "src": "assets/videos/dora/lampnorm.obj.mp4"}, {"mesh_src": "assets/samples/trellis/it0_lampnorm.obj.drc", "src": "assets/videos/trellis/it0_lampnorm.obj.mp4"}];
+
+function createElementFromHTML(htmlString) {
+    var div = document.createElement('div');
+    div.innerHTML = htmlString.trim();
+
+    // Change this to div.childNodes to support multiple top-level nodes.
+    return div.firstChild;
+}
+
+function add_videos(num_videos = 10) {
+    let thumbnails = document.getElementById("textToVideos");
+    let start = window.start;
+//   videoPaths = videoPaths.slice(0, 3);
+    for (let i = start; i < start + num_videos && i < videosAttributes.length; i++) {
+        let outer = document.createElement("DIV");
+        outer.classList = "w3-col l2 m4 vidContainer";
+        let videoAttributes = videosAttributes[i];
+        if (videoAttributes.caption) {
+            const caption = createElementFromHTML(`<p valign="middle" halign="middle" align="center">${videoAttributes.caption}</p>`);
+            outer.appendChild(caption);
+        }
+
+        const video = createElementFromHTML(`<video class="video" id="video${i}" loop muted playsinline
+            src="${videoAttributes.src}"></video>`);
+        video.addEventListener('loadedmetadata', function() {
+            resizeAndPlay(this);
+        });
+        video.play(); // 尝试自动播放
+        outer.appendChild(video);
+
+        let componentCanvas = document.createElement("CANVAS")
+        componentCanvas.height = "0";
+        componentCanvas.classList = "videoMerge";
+        componentCanvas.id = "video" + i + "Merge";
+        outer.appendChild(componentCanvas);
+
+        if (videoAttributes.mesh_src) {
+            let iframe = createElementFromHTML(`<iframe src="" class="meshViewer" frameBorder="0"></iframe>`);
+            let iframe_button = createElementFromHTML(`<a href="#" class="w3-button w3-light-grey w3-block viewerToggle" data-sequence="${videoAttributes.mesh_src}" ></a>`);
+            if (videoAttributes.nframes) {
+                iframe_button.dataset.nframes = videoAttributes.nframes;
+            }
+            add_button_logic(iframe_button);
+            outer.appendChild(iframe);
+            outer.appendChild(iframe_button)
+        } else {
+            let dummy = createElementFromHTML(`<div class="emptydiv"></div>`);
+            outer.appendChild(dummy);
+        }
+
+        thumbnails.appendChild(outer);
+        window.start = window.start + 1;
+    }
+    if (window.start >= videosAttributes.length) {
+        const load_more_button = document.getElementById("load_more");
+        load_more_button.remove();
+    }
+
+}
+function disableMeshViewer(iframe, canvas, a, textLabelMesh, video) {
+    iframe.src = '';
+    iframe.style.display = 'none';
+    canvas.style.display = 'none';
+    video.style.display = 'block';
+    a.textContent = textLabelMesh;
+}
+
+function enableMeshViewer(iframe, canvas, a, textLabelVideo, video, sequence, nframes) {
+    iframe.src = './render_draco.html?sequence=' + sequence;
+    if (nframes) {
+        iframe.src += '&nframes=' + nframes;
+    }
+    iframe.style.display = 'block';
+    video.style.display = 'none';
+    canvas.style.display = 'none';
+    a.textContent = textLabelVideo;
+}
+
+function add_button_logic(element) {
+    const textLabelMesh = 'Load mesh';
+    const textLabelVideo = 'Show video';
+    element.textContent = textLabelMesh;
+    
+    element.addEventListener('click', function (e) {
+        e.preventDefault();
+        const target = e.target;
+        const parent = target.parentElement;
+        const iframe = parent.querySelector('.meshViewer');
+        const canvas = parent.querySelector('.videoMerge');
+        const video = parent.querySelector('video');
+        
+        if (element.textContent === textLabelMesh) {
+            const sequence = target.dataset.sequence;
+            const nframes = target.dataset.nframes;
+            enableMeshViewer(iframe, canvas, target, textLabelVideo, video, sequence, nframes);
+            
+            const placeholder = parent.getElementsByClassName('animated-background');
+            if (placeholder.length > 0) {
+                placeholder[0].style.display = 'none';
+            }
+        } else {
+            disableMeshViewer(iframe, canvas, target, textLabelMesh, video);
+        }
+    });
+
+    return false;
+}
+
+// 移除全局的 activeElements
+window.start = 0;
+add_videos(num_videos = 20);

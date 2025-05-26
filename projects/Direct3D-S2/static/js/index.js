ceff506b211f03b1e976e0cd4dfca399e93bf241
Youtian Lin <linyoutian.loyot@gmail.com>
2025-05-26 11:57:31 +0800

    upd

--- projects/Direct3D-S2/static/js/index.js
+++ projects/Direct3D-S2/static/js/index.js
@@ -58,14 +58,6 @@ $(document).ready(function() {
     	});
     }
 
-    /*var player = document.getElementById('interpolation-video');
-    player.addEventListener('loadedmetadata', function() {
-      $('#interpolation-slider').on('input', function(event) {
-        console.log(this.value, player.duration);
-        player.currentTime = player.duration / 100 * this.value;
-      })
-    }, false);*/
-    preloadInterpolationImages();
 
     $('#interpolation-slider').on('input', function(event) {
       setInterpolationImage(this.value);
